e91cfcfe8cd1c64de337e1d55d691a33eac98c11
Tiago Costa <doriacosta.t@husky.neu.edu>
2019-08-26 13:56:38 -0400

    Add deploy scripts

---
+++ build/precache-manifest.3fbcd877c35e8e2a24c4ec2bd578eeb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc6ca167f2a92a7b2080f095572b356",
+    "url": "/optimistic-updates/index.html"
+  },
+  {
+    "revision": "cd3e229c541f5417b6ed",
+    "url": "/optimistic-updates/static/css/main.40b6ea22.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/optimistic-updates/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "cd3e229c541f5417b6ed",
+    "url": "/optimistic-updates/static/js/main.46a10d7c.chunk.js"
+  },
+  {
+    "revision": "aeb3e54554cc29b10b46",
+    "url": "/optimistic-updates/static/js/runtime~main.51d80f86.js"
+  }
+]);
